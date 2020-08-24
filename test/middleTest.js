a2a1bf39fef1376af69419fc657a5f1805a63893
Keith Ong <keith.ong5000@gmail.com>
2020-08-24 20:09:07 +0000

    Refactor middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,19 @@
 const assertArraysEqual = require('../assertArraysEqual')
 const middle = require('../middle')
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1]), [])
-assertArraysEqual(middle([1, 2]), [])
-assertArraysEqual(middle([1, 2, 3]), [2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle(["a", 'b', "c", "d"]), ["b", "c"])
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.strictEqual(assertArraysEqual(middle([1]), []), true);
+  });
+  it("returns '[]' for [1, 2]", () => {
+    assert.strictEqual(assertArraysEqual(middle([1, 2]), []), true);
+  });
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.strictEqual(assertArraysEqual(middle([1, 2, 3]), [2]), true);
+  });
+  it("returns [\"b\", \"c\"] for [\"a\", \"b\", \"c\", \"d\"]", () => {
+    assert.strictEqual(assertArraysEqual(middle(["a", 'b', "c", "d"]), ["b", "c"]), true);
+  });
+
+})
