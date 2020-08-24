c083f234fff0f786e4bd9ac7684a85e408a1e8aa
Keith Ong <keith.ong5000@gmail.com>
2020-08-24 19:32:31 +0000

    Refactor middle

--- middle.js
+++ middle.js
@@ -1,22 +1,5 @@
-let eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false
-    }
-  }
-  return true
-}
-
-let assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✓✓✓ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`xxx Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = require('./assertArraysEqual')
 
 const middle = function (array) {
   let result = []
@@ -33,9 +16,4 @@ const middle = function (array) {
   }
 
 }
-
-assertArraysEqual(middle([1]), [])
-assertArraysEqual(middle([1, 2]), [])
-assertArraysEqual(middle([1, 2, 3]), [2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle(["a", 'b', "c", "d"]), ["b", "c"])
+module.exports =  middle;
